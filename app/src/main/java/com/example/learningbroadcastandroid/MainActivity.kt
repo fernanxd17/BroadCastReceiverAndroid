08c1bbc9eab74138ec179e91666add52936630ff
Juan Fernando Romero Ortega <fernando.romero@ceiba.com.co>
2021-02-19 16:32:11 -0500

    Recibiendo emisiones a travez de context

--- app/src/main/java/com/example/learningbroadcastandroid/MainActivity.kt
+++ app/src/main/java/com/example/learningbroadcastandroid/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.learningbroadcastandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.learningbroadcastandroid.broadcast.MyBroadcastReceiver
 
 class MainActivity : AppCompatActivity() {
+
+    private val br: BroadcastReceiver = MyBroadcastReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+        }
+
+        registerReceiver(br, filter)
     }
 
 }
